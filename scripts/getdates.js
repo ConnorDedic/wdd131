e448d98d0652439c87327026f1899fac60df028d
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-04-24 15:35:02 -0600

    fixing some errors w. the year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-const date = new Date().getFullYear();
-console.log(date);
+const year = new Date().getFullYear();
+console.log(year);
 
-document.getElementById("currentyear").innerHTML = "&copy" + date + " Connor Dedic, Idaho"
+document.getElementById("currentyear").innerHTML = "&copy" + year + " Connor Dedic, Idaho"
 document.getElementById("lastmod").innerHTML = document.lastModified;
