39f30fd77a60cb4d86743f8dc16a8e95fb974826
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-04-24 15:26:22 -0600

    fixed JS in footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,5 @@
-document.getElementById("lastmod").innerHTML = document.lastModified;
+const date = new Date().getFullYear();
+console.log(date);
 
+document.getElementById("currentyear").innerHTML = "&copy" + date + " Connor Dedic, Idaho"
+document.getElementById("lastmod").innerHTML = document.lastModified;
