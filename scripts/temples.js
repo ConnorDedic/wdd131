c3a968ddf87549d1ddaf4aa31ccbff1d8ed0cf85
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-05-03 17:32:19 -0600

    wow this is hard

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,13 +1,4 @@
-// const hamButton = document.querySelector('#menu');
-// const navigation = document.querySelector('.navigation');
-
-// hamButton.addEventListener('click', () => {
-// 	navigation.classList.toggle('open');
-// 	hamButton.classList.toggle('open');
-// });
-
-// Function to toggle the mobile menu
-// function toggleMenu() {
-//     var menu = document.getElementById("menu");
-//     menu.classList.toggle("active");
-// }
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
