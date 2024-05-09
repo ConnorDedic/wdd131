462ec1eb1e88cb36f6968b0aea8593730ffd81ad
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-05-09 14:49:14 -0600

    added js and style updates

---
+++ scripts/place.js
@@ -0,0 +1,18 @@
+// defining variables for weather factors (temp in Celcius and wind speed in  km/h)
+const temp = 11;
+const windSpeed = 4.9;
+
+// Wind chill function
+function calculateWindChill(temp, windSpeed) {
+    return (13.12 + 0.6215 * temp -11.37 * windSpeed ** 0.16 + 0.3965 * temp * windSpeed **0.16);
+}
+
+if (temp >= 10 && windSpeed >= 4.8) {
+    wc =  calculateWindChill(temp, windSpeed);
+} else {
+    wc =  "N/A";
+}
+document.getElementById("tem").innerHTML = "Temperature: "+ temp + "&deg C"
+document.getElementById("cond").innerHTML = "Conditions: " + "Sunny"
+document.getElementById("wind").innerHTML = "Wind Speed: " + windSpeed + " km/h"
+document.getElementById("windChill").innerHTML = "Wind Chill: " + Math.round(wc) + "&deg C"
