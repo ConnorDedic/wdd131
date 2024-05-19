7c492a47ec540f9844c7f7b9e63362d18a2b31e1
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-05-18 23:10:27 -0600

    this isnt fun

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -2,3 +2,165 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("active");
 }
+const temples = [
+    {
+      templeName: "Aba Nigeria",
+      location: "Aba, Nigeria",
+      dedicated: "2005, August, 7",
+      area: 11500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+      templeName: "Manti Utah",
+      location: "Manti, Utah, United States",
+      dedicated: "1888, May, 21",
+      area: 74792,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      templeName: "Payson Utah",
+      location: "Payson, Utah, United States",
+      dedicated: "2015, June, 7",
+      area: 96630,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      templeName: "Yigo Guam",
+      location: "Yigo, Guam",
+      dedicated: "2020, May, 2",
+      area: 6861,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      templeName: "Washington D.C.",
+      location: "Kensington, Maryland, United States",
+      dedicated: "1974, November, 19",
+      area: 156558,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      templeName: "Lima Perú",
+      location: "Lima, Perú",
+      dedicated: "1986, January, 10",
+      area: 9600,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      templeName: "Mexico City Mexico",
+      location: "Mexico City, Mexico",
+      dedicated: "1983, December, 2",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+      templeName: "San Diego California",
+      location: "California",
+      dedicated: "25 April 1993",
+      area: 72000,
+      imageUrl:
+      "https://www.churchofjesuschrist.org/imgs/9fc6d53550ed3243fb8eca9ebceb284d4865e7db/full/320%2C/0/default"
+    }
+    {
+      templeName: "Richmond Virginia Temple",
+      location: "Virginia",
+      dedicated: "7 May 2023",
+      area: 39202,
+      imageUrl:
+      "https://www.churchofjesuschrist.org/imgs/8d15af22978011edae44eeeeac1e7b4cc85f1968/full/320%2C/0/default"
+    }
+    {
+      templeName: "Taylorsville Utah",
+      location: "Utah",
+      dedicated: "2 June 2024",
+      area: 73492,
+      imageUrl:
+      "https://www.churchofjesuschrist.org/imgs/08db56099dce11eeaccceeeeac1eec0900fabbe8/full/320%2C/0/default"
+    }
+  ];
+
+createTempleCard(temples);
+
+// const oldbutton = document.querySelector("#old");
+// oldbutton.addEventListener("click", () => {
+//   let oldTemples = temples.filter(temple => temple.location.filter)
+// })
+
+function createTempleCard() {
+  document.querySelector(".res-grid").innerHTML = "";
+  temples.forEach(temple => {
+    let card = document.createElement("section");
+		let name = document.createElement("h3");
+		let location = document.createElement("p");	
+		let dedicated = document.createElement("p");
+		let area = document.createElement("p");
+		let image = document.createElement("img");
+
+		name.textContent = temple.templeName;
+		location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+		dedicated.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+		area.innerHTML = `<span class="label">Area:</span> ${temple.area} sq. ft.`;
+		image.setAttribute("src" ,temple.imageUrl);
+		image.setAttribute("alt" ,`${temple.templeName} temple`);
+		image.setAttribute("loading", "lazy");
+
+		card.appendChild(name);
+		card.appendChild(location);
+		card.appendChild(dedicated);
+		card.appendChild(area);
+		card.appendChild(image);
+
+		document.querySelector(".res-grid").appendChild(card);
+  })
+}
+const homeLink = document.getElementById("home-link");
+const oldLink = document.getElementById("old-link");
+const newLink = document.getElementById("new-link");
+const largeLink = document.getElementById("large-link");
+const smallLink = document.getElementById("small-link");
+const heading = document.querySelector(" h3");
+
+createTemple(temples);
+
+// this code is  refering to a actual info when user click  the link
+homeLink.addEventListener("click", () => {
+  createTemple(temples);
+  heading.textContent = " Home ";
+});
+
+newLink.addEventListener("click", () => {
+  const newTemples = temples.filter((temple) => {
+    const year = new Date(temple.dedicated).getFullYear();
+    return year >= 2005 && year <= 2024;
+  });
+  createTemple(newTemples);
+  heading.textContent = "New"
+});
+
+oldLink.addEventListener("click", () => {
+	const newTemples = temples.filter((temple) => {
+	  const year = new Date(temple.dedicated).getFullYear();
+	  return  year < 2005;
+	});
+	createTemple(newTemples);
+	heading.textContent = "Old"
+  });
+
+largeLink.addEventListener("click", () => {
+  const largeTemples = temples.filter((temple) => temple.area >= 10000);
+  createTemple(largeTemples);
+  heading.textContent = "Large"
+});
+
+smallLink.addEventListener("click", () => {
+  const smallTemples = temples.filter((temple) => temple.area < 10000);
+  createTemple(smallTemples);
+  heading.textContent = "Small"
+});
+ 
