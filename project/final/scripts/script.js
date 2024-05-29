ac01237d79d8b0b10e5e4d072c2a70d85b2c9540
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-05-29 00:18:11 -0600

    style cahnges

---
+++ project/final/scripts/script.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+console.log(year);
+
+document.getElementById("currentyear").innerHTML = "&copy" + year + " &#129430 Connor Dedic, Idaho &#128050"
+document.getElementById("lastmod").innerHTML = document.lastModified;
