d89b7835aab03990a31fe4e257b1f6edf05f3689
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-06-01 15:36:57 -0600

    added hamburger menu

--- project/final/scripts/script.js
+++ project/final/scripts/script.js
@@ -4,3 +4,7 @@ console.log(year);
 document.getElementById("currentyear").innerHTML = "&copy" + year + " &#129430 Connor Dedic, Idaho &#128050"
 document.getElementById("lastmod").innerHTML = document.lastModified;
 
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
