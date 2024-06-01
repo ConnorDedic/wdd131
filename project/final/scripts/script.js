3770ad6ded2503e0bbe48e0e3bde9cb7280734e7
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-05-31 21:43:46 -0600

    added page 2

--- project/final/scripts/script.js
+++ project/final/scripts/script.js
@@ -3,3 +3,4 @@ console.log(year);
 
 document.getElementById("currentyear").innerHTML = "&copy" + year + " &#129430 Connor Dedic, Idaho &#128050"
 document.getElementById("lastmod").innerHTML = document.lastModified;
+
