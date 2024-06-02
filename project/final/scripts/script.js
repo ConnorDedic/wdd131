7915340ac5316e7817583aa6bf425c994bc2ac84
Connor Dedic <104650043+Enigma5656@users.noreply.github.com>
2024-06-01 23:06:43 -0600

    added localstorage to form

--- project/final/scripts/script.js
+++ project/final/scripts/script.js
@@ -8,3 +8,9 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("active");
 }
+let num_of_reviews = localStorage.getItem('num_of_reviews');
+  if (num_of_reviews) {
+      num_of_reviews = parseInt(num_of_reviews, 10);
+  } else {
+      num_of_reviews = 0;
+  }
